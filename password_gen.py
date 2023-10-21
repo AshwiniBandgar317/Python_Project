fc111bb7b19d1a13946c72927f370fc79b736724
AshwiniBandgar317 <148645589+AshwiniBandgar317@users.noreply.github.com>
2023-10-21 22:27:18 +0530

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,23 @@
+# import random
+import string
+import random
+
+if __name__ == '__main__':
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+
+    plen = int(input("enter password length:"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    random.shuffle(s)
+    # print(s)
+    print("your password is:")
+    print("".join(s[0:plen]))
+
+
